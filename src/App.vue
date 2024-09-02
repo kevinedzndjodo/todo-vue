f1250d1afb926c7c9d9608a387e1d144c333deb7
Kevin <iamfalash@gmail.com>
2024-09-02 20:56:01 +0100

    getting started wit vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ToDoItem from './components/ToDoItem.vue'
+</script>
+
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My Todo List"></to-do-item>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean }
+  }
+}
+</script>
